7dcaa5ef83f104f07bdb63ae15f998af0265b6e7
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-07-12 17:41:15 +0330

    feat: add diagnostic descriptors for configuration parsing
    Add diagnostic descriptors for handling parsing failures and missing dependencies in configuration processing. These descriptors will help users identify and resolve common issues when working with configuration extensions.

---
+++ src/SetSharp/Diagnostics/DiagnosticDescriptors.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SetSharp.Diagnostics
+{
+    internal static class DiagnosticDescriptors
+    {
+        internal static readonly DiagnosticDescriptor ParsingFailedError = new(
+           "SSG001", "App settings parsing failed", "{0}", "Error", DiagnosticSeverity.Error, true);
+
+        internal static readonly DiagnosticDescriptor MissingOptionsDependencyError = new(
+            "SSG002", "Missing Dependencies", "To generate IOptions extensions, the project must reference 'Microsoft.Extensions.Options.ConfigurationExtensions'. Please install the corresponding NuGet package.", "Usage", DiagnosticSeverity.Error, true);
+
+        internal static readonly DiagnosticDescriptor MissingBaseDependencyError = new(
+            "SSG003", "Missing Dependency", "The project must reference 'Microsoft.Extensions.Configuration.Abstractions' for basic functionality. Please install the corresponding NuGet package.", "Usage", DiagnosticSeverity.Error, true);
+    }
+}
