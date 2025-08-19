3d972a486fd925328fd40cfe90721feb0c996552
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-08-19 10:42:55 +0330

    feat: add MSBuildPropertyKeys class for property key definitions
    Introduce a new static class to centralize MSBuild property key constants with SetSharp prefix for better maintainability and consistency.

---
+++ src/SetSharp/Definitions/MSBuildPropertyKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SetSharp.Definitions
+{
+    internal static class MSBuildPropertyKeys
+    {
+        private const string _prefix = "build_property.";
+
+        public const string SetSharpPrefix = "SetSharp_";
+
+        public const string OptionPatternGenerationEnabled = _prefix + SetSharpPrefix + "OptionPatternGenerationEnabled";
+
+        public const string SourceFile = _prefix + SetSharpPrefix + "SourceFile";
+    }
+}
